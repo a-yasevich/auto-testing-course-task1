40437f9fdc969cf1e9274f4b3fdf83bfa858c691
n.dergunov <n.dergunov@vk.team>
2022-04-11 00:46:06 +0300

    ISSUE_FIX

--- src/test/java/pages/AbstractPage.java
+++ src/test/java/pages/AbstractPage.java
@@ -1,20 +1,25 @@
 package pages;
 
 import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 
 public abstract class AbstractPage {
-    private final SelenideElement userNavBlock = $x("//*[@id=\"hook_Block_SideNavigation\"]");
-    private final SelenideElement userCardMini = $x("//div[@class=\"ucard-mini toolbar_ucard js-toolbar-menu\"]");
+
+    private final static By USER_NAVIGATION_BLOCK = By.xpath("//*[@id=\"hook_Block_SideNavigation\"]");
+    private final static By USER_MINI_CARD = By.xpath("//div[@class=\"ucard-mini toolbar_ucard js-toolbar-menu\"]");
+    private final static By LOGOUT_LINK = By.xpath("//a[@data-l=\"t,logout\"]");
+    private final static By LOGOUT_BUTTON = By.xpath("//input[@data-l=\"t,logout\"]");
 
     public boolean hasUserNavBlock() {
-        return userNavBlock.exists();
+        return $(USER_NAVIGATION_BLOCK).exists();
     }
 
     public void logout() {
-        userCardMini.click();
-        $x("//a[@data-l=\"t,logout\"]").click();
-        $x("//input[@data-l=\"t,logout\"]").click();
+        $(USER_MINI_CARD).click();
+        $(LOGOUT_LINK).click();
+        $(LOGOUT_BUTTON).click();
     }
 }
