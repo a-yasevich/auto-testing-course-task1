c87977e4271ceb55f2ad6c1bf92dae29eedb0043
n.dergunov <n.dergunov@vk.team>
2022-04-11 00:46:06 +0300

    ISSUE_FIX

--- src/test/java/tests/FeedTest.java
+++ src/test/java/tests/FeedTest.java
@@ -9,20 +9,10 @@ import static org.junit.jupiter.api.Assertions.*;
 public class FeedTest extends BaseTest {
 
     @Test
-    public void logIn() {
-        assertAll("Main page has its distinctive traits",
-                () -> assertTrue(mainPage.hasUserNameField(), "There is no name field"),
-                () -> assertTrue(mainPage.hasPhoto(), "There is no photo block"),
-                () -> assertTrue(mainPage.hasFeedList(), "There is no feed list")
-        );
-        assertEquals(user.getName(), mainPage.getUserName(), "Wrong user");
-    }
-
-    @Test
     void addPost() {
         FeedItem feedItem = mainPage.doRepost();
         ProfilePage profilePage = mainPage.openProfilePage();
-        assertTrue(profilePage.hasFeedListUser());
+        profilePage.hasFeedListUser();
         assertEquals(feedItem, profilePage.getLastFeed());
     }
 }
