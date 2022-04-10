6dbf1b71661ce25c922824582960c9c820de8ffe
n.dergunov <n.dergunov@vk.team>
2022-04-11 01:10:24 +0300

    ISSUE_FIX

--- src/test/java/pages/elements/FeedList.java
+++ src/test/java/pages/elements/FeedList.java
@@ -2,26 +2,26 @@ package pages.elements;
 
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
 import utils.FeedItem;
 
-import static com.codeborne.selenide.Selenide.$$x;
-import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.*;
 
 public class FeedList {
-    private final SelenideElement feedList = $x("//*[@class=\"feed-list\"]");
+    private final static By FEED_LIST = By.xpath("//*[@class=\"feed-list\"]");
     private final ElementsCollection listOfPosts = $$x("//*[@class=\"feed-w\"]");
 
     public boolean hasFeedList() {
-        return feedList.exists();
+        return $(FEED_LIST).exists();
     }
 
     public FeedItem doRepost() {
         SelenideElement firstPost = listOfPosts.first();
-        SelenideElement button = $x("//*[@class=\"widget  __compact\"]/button");
-        button.click();
-        SelenideElement buttonRepost = $x("//*[@data-l=\"t,now\"]");
+        SelenideElement buttonRepost = $x("//*[@class=\"widget  __compact\"]/button");
+        buttonRepost.click();
+        SelenideElement buttonDoRepostNow = $x("//*[@data-l=\"t,now\"]");
         FeedItem feedItem = new FeedItem(firstPost.text(), $x("//a[@class=\"media-text_a\"]").attr("href"));
-        buttonRepost.scrollIntoView(false).click();
+        buttonDoRepostNow.scrollIntoView(false).click();
         return feedItem;
     }
 
