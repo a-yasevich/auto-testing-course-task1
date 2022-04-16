c729329f4c5a6bf702edf708d1885e7262a20528
n.dergunov <n.dergunov@vk.team>
2022-04-16 04:24:20 +0300

    Fix message tests

--- src/test/java/tests/MessageTest.java
+++ src/test/java/tests/MessageTest.java
@@ -2,7 +2,6 @@ package tests;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import pages.MessagePage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
